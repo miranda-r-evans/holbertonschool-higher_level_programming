7d3e74211537d5d5dfe88cb59f83818e32ca0959
mirandarevans <miranda.r.evans@gmail.com>
2018-04-10 21:52:49 +0000

    fixing minor issues

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,8 +2,8 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-    print("is positive")
+    print("{:d} is positive".format(number))
 elif number == 0:
-    print("is zero")
+    print("0 is zero")
 else:
-    print("is negative")
+    print("{:d} is negative".format(number))
--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,14 +2,18 @@
 import random
 number = random.randint(-10000, 10000)
 abs_num = number
+
+if number >= 0:
+    if number == 0 or number % 10 == 0:
+        print("Last digit of {:d} is 0 and is 0".format(number))
+    elif number % 10 > 5:
+        print("Last digit of {:d} is {:d} and is greater than 5".format
+              (number, number % 10))
+    else:
+        print("Last digit of {:d} is {:d} and is less than 6 and not 0".format
+              (number, number % 10))
+
 if number < 0:
     abs_num = number * -1
-
-if abs_num % 10 > 5:
-    print("Last digit of {:d} is {:d} and is greater than 5".format
-          (number, abs_num % 10))
-elif abs_num % 10 == 0:
-    print("Last digit of {:d} is {:d} and is 0".format(number, abs_num % 10))
-else:
-    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format
-          (number, abs_num % 10))
+    print("Last digit of -{:d} is -{:d} and is less than 6 and not 0".format
+          (abs_num, abs_num % 10))
