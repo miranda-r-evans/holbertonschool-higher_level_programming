516f425853287406fc38aeccb3879c3201a546fb
mirandarevans <miranda.r.evans@gmail.com>
2018-04-17 20:03:48 +0000

    fixed edge cases on 9 and 11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,7 +3,7 @@
 
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx > len(my_list) - 1:
-        return
+        return my_list
     tmp = []
     tmp.extend(my_list[:idx])
     tmp.extend(my_list[idx + 1:])
