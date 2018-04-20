0db6393d2a6b0af4052386b35e7fcd7bfec2394f
mirandarevans <miranda.r.evans@gmail.com>
2018-04-20 17:51:56 +0000

    fixed 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,16 +4,18 @@
 def roman_to_int(roman_string):
     if roman_string is None or isinstance(roman_string, str) is not True:
         return 0
-    sum = 0
-    biggest_val = 1
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+    dictionary = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
                   'D': 500, 'M': 1000}
+    total = 0
+    prev = 0
     for let in roman_string:
-        if let not in roman_dict:
+        if let not in dictionary:
             return 0
-        if roman_dict[let] > biggest_val:
-            biggest_val = roman_dict[let]
-            sum = roman_dict[let] - sum
+        if dictionary[let] > total:
+            total = dictionary[let] - total
+        elif dictionary[let] > prev:
+            total = total + dictionary[let] - prev * 2
         else:
-            sum = sum + roman_dict[let]
-    return sum
+            total = dictionary[let] + total
+        prev = dictionary[let]
+    return total
