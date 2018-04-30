c8dd16306a0e3fd7d1fe463ed23155abebeb087e
mirandarevans <miranda.r.evans@gmail.com>
2018-04-30 21:38:42 +0000

    fixing 3 and 4

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -8,3 +8,4 @@ def safe_print_division(a, b):
         c = None
     finally:
         print('Inside result: {}'.format(c))
+        return c
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -5,14 +5,16 @@ def list_division(my_list_1, my_list_2, list_length):
     new = []
     for index in range(list_length):
         try:
-            new.append(my_list_1[index] / my_list_2[index])
+            n = my_list_1[index] / my_list_2[index]
         except TypeError:
             print('wrong type')
-            new.append(0)
+            n = 0
         except ZeroDivisionError:
             print('division by 0')
-            new.append(0)
+            n = 0
         except IndexError:
             print('out of range')
-            new.append(0)
+            n = 0
+        finally:
+            new.append(n)
     return new
