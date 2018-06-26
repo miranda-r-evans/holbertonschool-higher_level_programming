71b6048daa7db58dd8da0201cdc546d787f17d08
mirandarevans <miranda.r.evans@gmail.com>
2018-06-26 08:52:06 -0700

    adding minor corrections to 2 and 15

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- lists number of each score
-SELECT score, COUNT(*) as number FROM second_table GROUP BY score;
+SELECT score, COUNT(*) as number FROM second_table GROUP BY score ORDER BY number ASC;
