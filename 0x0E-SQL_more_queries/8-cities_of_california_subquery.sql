4734eac58d474e1a04184ebc4c7f3eb414cb18a6
mirandarevans <miranda.r.evans@gmail.com>
2018-06-27 16:31:31 +0000

    adding correction to 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,8 @@
 -- lists all cities of California in hbtn_0d_usa
 SELECT id, name
-FROM hbtn_0d_usa.cities
+FROM cities
 WHERE state_id =
       (SELECT id
-      FROM hbtn_0d_usa.states
+      FROM states
       WHERE name = 'California')
 ORDER BY cities.id ASC;
