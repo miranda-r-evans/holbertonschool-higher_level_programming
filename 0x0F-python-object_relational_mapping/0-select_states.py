8012f39f64d2b9c62285e1673ffc5433f1992704
mirandarevans <miranda.r.evans@gmail.com>
2018-07-20 17:52:11 +0000

    trying to pass checker on 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,8 +8,8 @@ import sys
 
 
 if __name__ == "__main__":
-        db = MySQLdb.connect("localhost", sys.argv[1],
-                             sys.argv[2], sys.argv[3])
+        db = MySQLdb.connect(user=sys.argv[1],
+                             passwd=sys.argv[2], db=sys.argv[3])
 
         cursor = db.cursor()
 
