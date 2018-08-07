1b3c2bb887b65f5a907cfadf526796cd5d1c8708
mirandarevans <miranda.r.evans@gmail.com>
2018-08-06 17:25:18 -0700

    fixing formatting issue in 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,5 +9,5 @@ from requests import get
 if __name__ == "__main__":
         response = get('https://intranet.hbtn.io/status')
         print("Body response:")
-        print('\t - type:', response.reason.__class__)
-        print('\t - content:', response.reason)
+        print('\t- type:', response.reason.__class__)
+        print('\t- content:', response.reason)
