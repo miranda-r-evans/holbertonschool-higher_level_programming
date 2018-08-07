674e84993d1e130ac229b801c25583cb579a4d7d
mirandarevans <miranda.r.evans@gmail.com>
2018-08-06 17:21:07 -0700

    adding a line to stdout in 0 and 4

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,6 +9,7 @@ from urllib.request import urlopen
 if __name__ == "__main__":
         with urlopen('https://intranet.hbtn.io/status') as response:
                 message = response.read()
+                print("Body response:")
                 print('\t- type: {}'.format(message.__class__))
                 print('\t- content: {}'.format(message))
                 print('\t- utf8 content: {}'.format(response.msg))
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,5 +8,6 @@ from requests import get
 
 if __name__ == "__main__":
         response = get('https://intranet.hbtn.io/status')
+        print("Body response:")
         print('\t - type:', response.reason.__class__)
         print('\t - content:', response.reason)
