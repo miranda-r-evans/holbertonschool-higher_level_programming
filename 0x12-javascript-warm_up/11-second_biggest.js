4a17ade966315ed9333cd666e948517e1f849aa0
mirandarevans <miranda.r.evans@gmail.com>
2018-08-28 14:04:14 -0700

    fixing incorrect use of splice method on 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,7 @@ myArray.shift();
 myArray.forEach(function (element, index) {
   myArray[index] = parseInt(element);
 });
-myArray.splice(myArray.indexOf(Math.max(...myArray)));
+myArray.splice(myArray.indexOf(Math.max(...myArray)), 1);
 if (myArray.length === 0) {
   console.log(0);
 } else {
