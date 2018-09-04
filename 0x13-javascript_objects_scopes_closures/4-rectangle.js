6ed728c5ba8a8e88d9fbc6de242c605c39a138a2
mirandarevans <miranda.r.evans@gmail.com>
2018-09-04 13:10:57 -0700

    adding 4 and 5

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let r = ('X'.repeat(this.width) + '\n').repeat(this.height);
+    process.stdout.write(r);
+  }
+
+  rotate() {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double() {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
