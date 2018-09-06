55f2114369a9d7eb2db2679f861aff6299140c92
mirandarevans <miranda.r.evans@gmail.com>
2018-09-05 18:44:39 -0700

    fixing flaw in logic

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -10,10 +10,12 @@ request(process.argv[2], function (err, response, body) {
   let dict = {};
   let resBody = JSON.parse(body);
   for (let i in resBody) {
-    if (dict[resBody[i]['userId']]) {
-      dict[resBody[i]['userId']]++;
-    } else {
-      dict[resBody[i]['userId']] = 1;
+    if (resBody[i]['completed'] === true) {
+      if (dict[resBody[i]['userId']]) {
+        dict[resBody[i]['userId']]++;
+      } else {
+        dict[resBody[i]['userId']] = 1;
+      }
     }
   }
   console.log(dict);
