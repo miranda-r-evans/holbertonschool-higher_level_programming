a6e42a32f46ee2c182985c3e4d356c4fa4474bdd
mirandarevans <miranda.r.evans@gmail.com>
2018-09-06 09:52:04 -0700

    taking out '/.json' from url

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 
-request(process.argv[2] + '/.json', function (err, response, body) {
+request(process.argv[2], function (err, response, body) {
   if (err) {
     console.log(err);
     return;
